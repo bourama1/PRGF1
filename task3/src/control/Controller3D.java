4bcd89ed25a4a4bea8238133ffddfc50d71b90a0
bourama1 <bourama1@uhk.cz>
2020-12-08 23:12:07 +0100

    Update Controller3D and Renderer

--- task3/src/control/Controller3D.java
+++ task3/src/control/Controller3D.java
@@ -1,59 +1,52 @@
 package control;
 
-import clip.Clipper;
-import fill.ScanLine;
-import fill.SeedFill;
-import fill.SeedFillBorder;
-import model.Point;
-import model.Polygon;
 import model.Scene;
-import rasterize.DashLineRasterizer;
 import rasterize.FilledLineRasterizer;
-import rasterize.PolygonRasterizer;
 import rasterize.Raster;
-import solids.*;
-import view.Panel;
 import render.Renderer;
+import solids.Axis;
+import solids.Box;
+import solids.Tetrahedron;
+import transforms.*;
+import view.Panel;
 
-import javax.swing.SwingUtilities;
-import java.awt.*;
 import java.awt.event.*;
 
 public class Controller3D implements Controller {
 
     private final Panel panel;
 
-    private int x,y, n = 0;
-    private boolean dashedPolygon = false;
-    private SeedFill seedFill;
-    private SeedFillBorder seedFillBorder;
-    private ScanLine scanLine;
-    private final Polygon polygon = new Polygon();
-    private final Polygon clipPolygon = new Polygon();
-    private Polygon outPolygon = new Polygon();
-    private PolygonRasterizer polygonRasterizer;
+    private int x, y, h, w;
+    private float angle;
     private Scene scene;
     private Renderer renderer;
     private FilledLineRasterizer lineRasterizer;
+    private Camera camera = new Camera()
+            .withPosition(new Vec3D(-5, 0 ,0))
+            .withAzimuth(0)
+            .withZenith(0);
 
     public Controller3D(Panel panel) {
         this.panel = panel;
         initObjects(panel.getRaster());
         initListeners(panel);
-        draw();
+        update();
     }
 
     public void initObjects(Raster raster) {
         lineRasterizer = new FilledLineRasterizer(raster);
         scene = new Scene();
-        renderer = new Renderer(lineRasterizer, raster.getImg());
-        seedFill = new SeedFill(raster);
-        seedFillBorder = new SeedFillBorder(raster);
-        scanLine = new ScanLine(raster);
-        polygonRasterizer = new PolygonRasterizer(lineRasterizer, new DashLineRasterizer(raster));
+        renderer = new Renderer(lineRasterizer, raster.getImg(), raster);
+        h = raster.getHeight();
+        w = raster.getWidth();
     }
 
-    public void draw(){
+    public void update(){
+        panel.clear();
+        Mat4 model = new Mat4RotXYZ(angle * Math.PI/10, Math.PI/5, Math.PI/7);
+        renderer.setProjection(new Mat4PerspRH(Math.PI/2, h/(double)w, 0.1, 10));
+        renderer.setView(camera.getViewMatrix());
+
         scene.getSolids().add(new Axis());
         scene.getSolids().add(new Tetrahedron());
         scene.getSolids().add(new Box());
@@ -66,38 +59,15 @@ public class Controller3D implements Controller {
 
             @Override
             public void mouseReleased(MouseEvent e) {
-                if (e.isShiftDown()) {
-                    if (SwingUtilities.isMiddleMouseButton(e)) {
-                        seedFillBorder.setSeed(e.getX(), e.getY());
-                        seedFillBorder.setBorderColor(Color.CYAN);
-                        seedFillBorder.fill();
-                    }
-                } else if (SwingUtilities.isLeftMouseButton(e)) {
-                    Point p = new Point(e.getX(), e.getY());
-                    polygon.addPoint(p);
-                    updatePolygon();
-                } else if (SwingUtilities.isMiddleMouseButton(e)) {
-                    seedFill.setSeed(e.getX(), e.getY());
-                    seedFill.fill();
-                } else if (SwingUtilities.isRightMouseButton(e)) {
-                    Point p = new Point(e.getX(), e.getY());
-                    clipPolygon.addPoint(p);
-                    updatePolygon();
-                }
+
             }
         });
 
         panel.addMouseMotionListener(new MouseAdapter() {
+
             @Override
             public void mouseDragged(MouseEvent e) {
-                if (SwingUtilities.isLeftMouseButton(e)) {
-                    if (polygon.getPointsSize() > 0) {
-                        x = e.getX();
-                        y = e.getY();
-                        dashedPolygon = true;
-                        updatePolygon();
-                    }
-                }
+
             }
         });
 
@@ -105,7 +75,7 @@ public class Controller3D implements Controller {
             @Override
             public void keyPressed(KeyEvent e) {
                 if (e.getKeyCode() == KeyEvent.VK_R) {
-                    reset();
+                    update();
                 }
             }
         });
@@ -117,36 +87,5 @@ public class Controller3D implements Controller {
                 initObjects(panel.getRaster());
             }
         });
-    }
-
-    private void updatePolygon() {
-        hardClear();
-
-        if (dashedPolygon)
-            polygonRasterizer.rasterizeDash(polygon, x, y);
-        dashedPolygon = false;
-
-        if (clipPolygon.getPointsSize() >= 3) {
-            outPolygon = Clipper.clip(polygon, clipPolygon);
-            polygonRasterizer.rasterize(outPolygon, Color.WHITE);
-            scanLine.setFillColor(Color.WHITE.getRGB());
-            scanLine.setOutlineColor(Color.WHITE);
-            scanLine.setPoly(outPolygon);
-            scanLine.fill();
-        }
-        if (polygon.getPointsSize() > 0)
-            polygonRasterizer.rasterize(polygon, Color.CYAN);
-        if (clipPolygon.getPointsSize() > 0)
-            polygonRasterizer.rasterize(clipPolygon, Color.RED);
-    }
-
-    private void hardClear() {
-        panel.clear();
-    }
-
-    private void reset() {
-        hardClear();
-        scene.clear();
-        draw();
     }
 }
--- task3/src/render/Renderer.java
+++ task3/src/render/Renderer.java
@@ -4,72 +4,73 @@ import model.Scene;
 import model.Solid;
 import model.Vertex;
 import rasterize.LineRasterizer;
+import rasterize.Raster;
 import transforms.Mat4;
+import transforms.Mat4Identity;
 import transforms.Point3D;
 import transforms.Vec3D;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Renderer {
     LineRasterizer lineRasterizer;
+    Raster raster;
     BufferedImage bufferedImage;
-    private Mat4 model;
-    private Mat4 view;
-    private Mat4 projection;
+    private Mat4 model = new Mat4Identity();
+    private Mat4 view = new Mat4Identity();
+    private Mat4 projection = new Mat4Identity();
     private boolean clip;
 
-    public Renderer(LineRasterizer lineRasterizer, BufferedImage bufferedImage){
+    public Renderer(LineRasterizer lineRasterizer, BufferedImage bufferedImage, Raster raster){
         this.lineRasterizer = lineRasterizer;
+        this.raster = raster;
         this.bufferedImage = bufferedImage;
         clip = false;
     }
 
+    public void setModel(Mat4 model) {
+        this.model = model;
+    }
+
+    public void setProjection(Mat4 projection) {
+        this.projection = projection;
+    }
+
+    public void setView(Mat4 view) {
+        this.view = view;
+    }
+
     public void render(Scene scene){
-        //axis
-        render(scene.getSolids().get(0));
-
-        //tetra
-        render(scene.getSolids().get(1));
-
-        //box
-        render(scene.getSolids().get(2));
+        for (Solid solid: scene.getSolids()) {
+            render(solid);
+        }
     }
 
     public void render(Solid solid){
         int size = solid.getIndices().size();
-        for (int i = 0; i < size; i += 2){
-            if(size != 6)
-                renderLine(solid.getVertices().get(solid.getIndices().get(i)).getPosition(),
-                    solid.getVertices().get(solid.getIndices().get(i + 1)).getPosition(),
-                    Color.CYAN.getRGB(), clip, bufferedImage);
-            else
-                renderAxes(solid.getVertices().get(solid.getIndices().get(i)).getPosition(),
-                        solid.getVertices().get(solid.getIndices().get(i + 1)).getPosition(),
-                        solid.getColor().get(i / 2), clip, bufferedImage);
+        boolean axis = false;
+        if(size == 6)
+            axis = true;
+
+        Mat4 m = model.mul(view).mul(projection);
+        List<Vertex> transformedVerticies = new ArrayList<Vertex>();
+
+        for (Vertex v: solid.getVertices()) {
+            transformedVerticies.add(v.transform(m));
         }
-        /*Vertex a = new Vertex(0,0,0);
 
-        Vertex a2 = a.transform(model).transform(view).transform(projection); //n*(3*12+3*16)
-        // + : n*3*12 = 36
-        // * : n*3*16 = 48
+        for (int i = 0; i < size; i += 2){
+            renderLine(transformedVerticies.get(solid.getIndices().get(i)).getPosition(),
+                    transformedVerticies.get(solid.getIndices().get(i + 1)).getPosition(),
+                    axis? solid.getColor().get(i / 2) : Color.CYAN.getRGB(),
+                    clip, bufferedImage, axis);
+        }
 
-        Mat4 m = model.mul(view).mul(projection); //(A*B*C = A*(B*C) = (A*B)*C
-        Mat4 m2 = model.mul(view.mul(projection));
-        Mat4 m3 = (model.mul(view)).mul(projection);
 
-        a2 = a.transform(m); //n*(12+16)+2*48 +2*64
-        // + : n*12 + 2*48 = 108
-        // * : n*16 + 2*64 = 144
 
-        /* Mat4*Mat4
-            + : 3*4*4 = 48
-            * : 4*4*4 = 64
-           Poin3D*Mat4 - transform
-           + : 3*4 = 12
-           * : 4*4 = 16
-        */
         //modelovaci transformace
         //pohledova transformace
         //projekcni (transformace zobrazovaciho objemu)
@@ -78,82 +79,47 @@ public class Renderer {
         //dehomogenizace
         //projekce
         //viewport transformace
-        //vykresleni pomoci linerasterizeru*/
+        //vykresleni pomoci linerasterizeru
     }
 
-    private void renderAxes(Point3D pA, Point3D pB, int color, boolean clip, BufferedImage bufferedImage) {
-        if (Math.min(pA.getW(), pB.getW()) < 0.0D)
-            return;
-        Optional<Vec3D> a = pA.dehomog();
-        Optional<Vec3D> b = pB.dehomog();
-        if (clip && (Math.min(a.get().getX(), b.get().getX()) < -1.0D || Math.max(a.get().getX(), b.get().getX()) > 1.0D ||
-                Math.min(a.get().getY(), b.get().getY()) < -1.0D || Math.max(a.get().getY(), b.get().getY()) > 1.0D ||
-                Math.min(a.get().getZ(), b.get().getZ()) < 0.0D || Math.max(a.get().getZ(), b.get().getZ()) > 1.0D))
-            return;
-        a = Optional.ofNullable(viewPort(a, bufferedImage));
-        b = Optional.ofNullable(viewPort(b, bufferedImage));
-        Graphics g = bufferedImage.getGraphics();
-        g.setColor(new Color(color));
-        g.drawLine((int)a.get().getX(), (int)a.get().getY(), (int)b.get().getX(), (int)b.get().getY());
-        if (color == 255)
-            g.drawString("x", (int)b.get().getX(), (int)b.get().getY());
-        if (color == 65280)
-            g.drawString("y", (int)b.get().getX(), (int)b.get().getY());
-        if (color == 16711680)
-            g.drawString("z", (int)b.get().getX(), (int)b.get().getY());
-    }
-
-    protected void renderLine(Point3D pA, Point3D pB, int color, boolean clip, BufferedImage bufferedImage) {
+    protected void renderLine(Point3D pA, Point3D pB, int color, boolean clip, BufferedImage bufferedImage, boolean axis) {
         int size = 3;
         if (Math.min(pA.getW(), pB.getW()) < 0.0D)
             return;
-        Optional<Vec3D> a = pA.dehomog();
-        Optional<Vec3D> b = pB.dehomog();
-        if (clip && (Math.min(a.get().getX(), b.get().getX()) < -1.0D || Math.max(a.get().getX(), b.get().getX()) > 1.0D ||
-                Math.min(a.get().getY(), b.get().getY()) < -1.0D || Math.max(a.get().getY(), b.get().getY()) > 1.0D ||
-                Math.min(a.get().getZ(), b.get().getZ()) < 0.0D || Math.max(a.get().getZ(), b.get().getZ()) > 1.0D))
+
+        //Dehomogenizace
+        Vec3D va = new Vec3D();
+        if(pA.dehomog().isPresent())
+            va = pA.dehomog().get();
+        Vec3D vb = new Vec3D();
+        if(pB.dehomog().isPresent())
+            vb = pB.dehomog().get();
+
+        if (clip && (Math.min(va.getX(), vb.getX()) < -1.0D || Math.max(va.getX(), vb.getX()) > 1.0D ||
+                Math.min(va.getY(), vb.getY()) < -1.0D || Math.max(va.getY(), vb.getY()) > 1.0D ||
+                Math.min(va.getZ(), vb.getZ()) < 0.0D || Math.max(va.getZ(), vb.getZ()) > 1.0D))
             return;
-        a = Optional.ofNullable(viewPort(a, bufferedImage));
-        b = Optional.ofNullable(viewPort(b, bufferedImage));
+
+        //ViewPort
+        int x1 = (int) ((va.getX() + 1) * (raster.getWidth() - 1 ) / 2);
+        int y1 = (int) ((va.getY() + 1) * (raster.getWidth() - 1 ) / 2);
+        int x2 = (int) ((vb.getX() + 1) * (raster.getWidth() - 1 ) / 2);
+        int y2 = (int) ((vb.getY() + 1) * (raster.getWidth() - 1 ) / 2);
+
         Graphics g = bufferedImage.getGraphics();
         g.setColor(new Color(color));
-        g.drawLine((int)a.get().getX(), (int)a.get().getY(), (int)b.get().getX(), (int)b.get().getY());
-        g.drawOval((int)a.get().getX() - size, (int)a.get().getY() - size, 2 * size, 2 * size);
-        g.drawOval((int)b.get().getX() - size, (int)b.get().getY() - size, 2 * size, 2 * size);
-    }
-
-    protected Vec3D viewPort(Optional<Vec3D> a, BufferedImage bufferedImage) {
-        int w = bufferedImage.getWidth();
-        int h = bufferedImage.getHeight();
-        int px = 0;
-        int py = 0;
-        switch (1) {
-            case 1:
-                px = 0;
-                py = 0;
-                w = bufferedImage.getWidth() / 2;
-                h = bufferedImage.getHeight() / 2;
-                break;
-            case 2:
-                px = 2;
-                py = 0;
-                w = bufferedImage.getWidth() / 2;
-                h = bufferedImage.getHeight() / 2;
-                break;
-            case 3:
-                px = 0;
-                py = 2;
-                w = bufferedImage.getWidth() / 2;
-                h = bufferedImage.getHeight() / 2;
-                break;
-            case 4:
-                px = 2;
-                py = 2;
-                w = bufferedImage.getWidth() / 2;
-                h = bufferedImage.getHeight() / 2;
-                break;
+        //lineRasterizer.rasterize(x1, y1, x2, y2, new Color(color));
+        g.drawLine(x1, y1, x2, y2);
+        if (axis){
+            if (color == 255)
+                g.drawString("x", x2, y2);
+            if (color == 65280)
+                g.drawString("y", x2, y2);
+            if (color == 16711680)
+                g.drawString("z", x2, y2);
+        } else {
+            g.drawOval(x1 - size, y1 - size, 2 * size, 2 * size);
+            g.drawOval(x2 - size, y2 - size, 2 * size, 2 * size);
         }
-        return a.get().mul(new Vec3D(1.0D, -1.0D, 1.0D)).add(new Vec3D((1 + px), (1 + py), 0.0D))
-                .mul(new Vec3D(((w - 1) / 2.0D), ((h - 1) / 2.0D), 1.0D));
     }
 }
